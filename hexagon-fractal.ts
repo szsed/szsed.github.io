89aa7105529b73fe54f73f5c914115d79584d0ca
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-17 23:24:15 +0200

    hexagon fractal

--- hexagon-fractal.ts
+++ hexagon-fractal.ts
@@ -5,15 +5,18 @@ const ctx = canvas.getContext('2d');
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
-const numberOfLayersHex: number = 4;
+const numberOfLayersHex: number = 5;
 const startCoordHex: [number, number] = [canvas.width / 2, canvas.height / 2];
 const hexSize: number = canvas.width / 2;
 
-let calcNextCenters = (inputCoord: [number, number], nextLayerSize: number): [number, number][] => {
+let calcNextCenters = (inputCoord: [number, number], inputLayerSize: number): [number, number][] => {
     let output: [number, number][] = [];
-    output.push([inputCoord[0] + nextLayerSize, inputCoord[1]]);
-    output.push([inputCoord[0] - nextLayerSize / 2, inputCoord[1] + nextLayerSize * 3 ** 0.5 / 2]);
-    output.push([inputCoord[0] - nextLayerSize / 2, inputCoord[1] - nextLayerSize * 3 ** 0.5 / 2]);
+    output.push([inputCoord[0] + inputLayerSize * 2 / 3, inputCoord[1]]);
+    output.push([inputCoord[0] + inputLayerSize * 1 / 3, inputCoord[1] + inputLayerSize * 3 ** 0.5 / 3]);
+    output.push([inputCoord[0] - inputLayerSize * 1 / 3, inputCoord[1] + inputLayerSize * 3 ** 0.5 / 3]);
+    output.push([inputCoord[0] - inputLayerSize * 2 / 3, inputCoord[1]]);
+    output.push([inputCoord[0] - inputLayerSize * 1 / 3, inputCoord[1] - inputLayerSize * 3 ** 0.5 / 3]);
+    output.push([inputCoord[0] + inputLayerSize * 1 / 3, inputCoord[1] - inputLayerSize * 3 ** 0.5 / 3]);
     return output;
 }
 
@@ -32,7 +35,7 @@ let drawHexFractal = (centerCoord: [number, number], layers: number, size: numbe
         ctx.lineTo(centerCoord[0] - size, centerCoord[1]);
         ctx.closePath();
         ctx.stroke();
-        calcNextCenters(centerCoord, size / 2).forEach((element) => drawHexFractal(element, layers - 1, size / 2));
+        calcNextCenters(centerCoord, size).forEach((element) => drawHexFractal(element, layers - 1, size / 3));
     }
 }
 
